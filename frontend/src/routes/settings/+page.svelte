3b68fc1517400778f9da5b3996ba2c46f6ba755d
Wollaston <matt@wollaston.dev>
2024-06-04 11:22:39 -0400

    Add settings page

---
+++ frontend/src/routes/settings/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<div class="flex justify-center align-middle">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>Tip Calculator</Card.Title>
+			<Card.Description>The Curious Cork Tip Pool Calculator</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<p>Please contact admin@curiouscorkwine.com with questions and comments</p>
+		</Card.Content>
+	</Card.Root>
+</div>
